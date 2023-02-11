e35f673c371b6e75cd0449f722c296f2fa21427b
Yuriy Mironenko <ydtalel@gmail.com>
2023-02-12 04:05:01 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import csv
+import re
+from prettytable import PrettyTable
+
+x = PrettyTable()
+
+
+def make_unique(list_, num1, num2):
+    list_.append([i or j for i, j in zip(list_[num1], list_[num2])])
+    del list_[num1]
+    del list_[num2 - 1]
+    return list_
+
+
+def sort_data(cont_list):
+    lst = []
+    for column in cont_list[1:]:
+        if " " in column[0]:
+            fullname = column[0].split()
+            for word in range(len(fullname)):
+                column[word] = fullname[word]
+        elif " " in column[1]:
+            fullname = column[1].split()
+            for word in range(len(fullname)):
+                column[word + 1] = fullname[word]
+        lst.append(column)
+
+    lst = make_unique(lst, 1, 3)
+    lst = make_unique(lst, 4, 5)
+    pattern_phone = r"(\+7|8)\s?\(?(\d{3})?\)?\W?(\d{3})\W?(\d{2})\W?(\d{2})\s\(?(доб.\s\d{4})?\)?"
+    substitution = r'+7(\2)\3-\4-\5\6'
+
+    for el in lst:
+        el[5] = re.sub(pattern_phone, substitution, f" {el[5]} ")
+    x.field_names = title
+    x.add_rows(lst)
+    print(x)
+    return lst
+
+
+if __name__ == '__main__':
+    with open("phonebook_raw.csv", encoding='utf-8') as f:
+        rows = csv.reader(f, delimiter=",")
+        contacts_list = list(rows)
+        title = contacts_list[0]
+
+    # # TODO 1: выполните пункты 1-3 ДЗ
+    lst_ = sort_data(cont_list=contacts_list)
+
+    # # TODO 2: сохраните получившиеся данные в другой файл
+    # код для записи файла в формате CSV
+    with open("phonebook_res.csv", "w", encoding="utf-8") as f:
+        datawriter = csv.writer(f, delimiter=',')
+        # Вместо contacts_list подставьте свой список
+        datawriter.writerows(lst_)
